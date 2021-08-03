cf795e874875752d8ae87904fa0f18d7a0b43ffe
brad-morrison <bradley_morrison@outlook.com>
2021-08-04 00:46:57 +0100

    More work on hero component for home screen

--- mscproject/src/components/Hero.js
+++ mscproject/src/components/Hero.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import heroPlaceholder from '../images/hero.jpg'
 
 function Hero() {
     return (
         <div className='wrapper'>
             <div className='hero-items'>
-                <h1>Visualising Code</h1>
+                <h1>Visualizing Code</h1>
                 <h3>Learn the abstract concepts of coding!</h3>
+                <button>Get Started!</button>
+                
             </div>
+            <img className='image-splash' src={heroPlaceholder} alt="hero placeholder" />
         </div>
     )
 }
