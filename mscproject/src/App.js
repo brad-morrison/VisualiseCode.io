1a9d84dec1b82a6d67b3e1a4ee65154f698521b4
brad-morrison <bradley_morrison@outlook.com>
2021-08-04 00:21:04 +0100

    Began work on Hero component

--- mscproject/src/App.js
+++ mscproject/src/App.js
@@ -1,17 +1,10 @@
 import './App.css';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h5 className="header">MSc Computing with International Business</h5>
-        <h5 className="header">Final Project</h5>
-        <h5 className="header">Helping Novice Programmers to Visualize Code.</h5>
-        <p className="text">
-          This will be a React JS app/website that will teach the fundamental basics of coding, 
-          and will integrate with Unity for the addition of mini-games and interactive sections.
-        </p>
-      </header>
+      <Hero/>
     </div>
   );
 }
---
+++ mscproject/src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hero.css'
+
+function Hero() {
+    return (
+        <div className='wrapper'>
+            <div className='hero-items'>
+                <h1>Visualising Code</h1>
+                <h3>Learn the abstract concepts of coding!</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
