804f797b51fcf4d2e0912cdd8672a4cad9b67090
brad-morrison <bradley_morrison@outlook.com>
2021-08-03 17:31:22 +0100

    tests

--- mscproject/src/App.js
+++ mscproject/src/App.js
@@ -4,8 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          base
+        <h5 className="header">MSc Computing with International Business</h5>
+        <h5 className="header">Final Project</h5>
+        <h5 className="header">Helping Novice Programmers to Visualize Code.</h5>
+        <p className="text">
+          This will be a React JS app/website that will teach the fundamental basics of coding, 
+          and will integrate with Unity for the addition of mini-games and interactive sections.
         </p>
       </header>
     </div>
