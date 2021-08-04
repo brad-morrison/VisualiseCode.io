28d33fbeb8575ba21d0ed2ca99c182ae98c1e881
brad-morrison <bradley_morrison@outlook.com>
2021-08-04 02:09:41 +0100

    Additions to UserBar

--- mscproject/src/components/UserBar.js
+++ mscproject/src/components/UserBar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './UserBar.css'
 import { FaUserCircle } from 'react-icons/fa'
+import { BsFillStarFill } from 'react-icons/bs'
 
 function UserBar() {
     return (
         <div className='userbar-wrapper'>
             <FaUserCircle className='user-icon'/>
-            <p className='user-text'>brad-morrison</p>
+            <div className='column-text'>
+                <p className='user-text'>brad-morrison</p>
+                <p className='skill-text'>Advanced</p>
+            </div>
+            <BsFillStarFill className='star-icon'/>
+            <p className='star-text'>3829</p>
         </div>
     )
 }
