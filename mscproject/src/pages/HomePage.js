12caef0eced4e857306373309e9ca37dba1ef8f6
brad-morrison <bradley_morrison@outlook.com>
2021-08-08 23:22:09 +0100

    Added CodeBlock component

--- mscproject/src/pages/HomePage.js
+++ mscproject/src/pages/HomePage.js
@@ -2,6 +2,13 @@ import React from 'react'
 import Hero from '../components/Hero'
 import UserBar from '../components/UserBar'
 import ImageWithText from '../components/ImageWithText'
+import Code from '../components/Code'
+
+const code = `// this is a comment
+moveForward();
+attack();
+run();
+`;
 
 function HomePage() {
     return (
@@ -9,6 +16,8 @@ function HomePage() {
             <UserBar/>
             <Hero/>
             <ImageWithText/>
+            <Code code={code} language="javascript" />
+            <Code code={code} language="javascript" />
             <div className='spacer'></div>
         </div>
     )
