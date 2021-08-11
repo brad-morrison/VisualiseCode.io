45c5cce286ae689d43143e9ff4f875c948ab0915
brad-morrison <bradley_morrison@outlook.com>
2021-08-11 06:31:22 +0100

    Added React typing to Unity Functionality and added some styling.

--- src/components-unity/UnityWeatherApp.js
+++ src/components-unity/UnityWeatherApp.js
@@ -33,8 +33,15 @@ function rain() {
     rainOff();
     day();
     night();
-    storm();
+    stormOn();
+    stormOff();
   `;
+
+
+  var dayOn = true;
+  var windOn = false;
+  var rainOn = false;
+  var stormOn = false;
 
   
 
@@ -43,9 +50,65 @@ function UnityWeatherApp() {
 
     const [userInput, setTitle] = useState('');
 
+    function action() {
+
+      setTitle('');
+
+      if (userInput == "windOn()" && windOn == false)
+      {
+        wind();
+        windOn = true;
+      }
+
+      if (userInput == "windOff()" && windOn == true)
+      {
+        wind();
+        windOn = false;
+      }
+
+      if (userInput == "rainOn()" && rainOn == false)
+      {
+        rain();
+        rainOn = true;
+      }
+
+      if (userInput == "rainOff()" && rainOn == true)
+      {
+        rain();
+        rainOn = false;
+      }
+
+      if (userInput == "day()" && dayOn == false)
+      {
+        day();
+        dayOn = true;
+      }
+
+      if (userInput == "night()" && dayOn == true)
+      {
+        day();
+        dayOn = false;
+      }
+
+      if (userInput == "stormOn()" && stormOn == false)
+      {
+        if (windOn == false){ wind(); windOn = true;}
+        if (rainOn == false){ rain(); rainOn = true;}
+        stormOn = true;
+      }
+
+      if (userInput == "stormOff()" && stormOn == true)
+      {
+        if (windOn == true){ wind(); windOn = false;}
+        if (rainOn == true){ rain(); rainOn = false;}
+        stormOn = false;
+      }
+    }
+    
     return (
-        <div className="p-d-flex p-jc-center card">
-            <div className="p-mr-5">
+      <div className="p-d-flex p-jc-center p-mb-5">
+        <div className="p-d-inline-flex p-flex-column p-flex-md-row p-jc-center card">
+            <div className="p-as-center">
                 <Unity unityContext={weather} 
                 style={{
                     height: "600px",
@@ -54,25 +117,28 @@ function UnityWeatherApp() {
                 />
             </div>
             <div className="">
-                <Panel header="Change the weather...">
+                <Panel header="Change the weather..." className="p-p-4">
                 <p>Using the appropriate method names - try to change the weather in the forest scene.</p>
                 <p>!Hint - Remember the ';'!</p>
                 
-                <Divider type="dashed"/>
+                <Divider type="dashed" className=""/>
                 
-                <Code code={code} language="javascript" />
+                <Code code={code} language="javascript"/>
                 
-                <form>
-                <InputText value={userInput} onChange={(e) => setTitle(e.target.value)} />
-                </form>
+                <div className="p-d-flex p-flex-md-row p-jc-center">
+                  <form>  
+                  <InputText value={userInput} onChange={(e) => setTitle(e.target.value)}  className="p-m-2"/>
+                  </form>
+                  <Button label="Run command" icon="pi pi-play" onClick={action} className="p-m-2" />
+                </div>
+                
 
-
-                <Button onClick={wind}>Wind</Button>
-                
                 </Panel>
             </div>
             
         </div>
+      </div>
+        
     )
 }
 
